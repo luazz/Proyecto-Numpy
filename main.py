6c2be8ab793247fc6563300228040b5bf0775c80
Luko <derosa.lucas@gmail.com>
2021-09-09 18:09:51 -0300

    Comienzo de actividades de Pandas

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import Funciones as fs
+import pandas as pd
+
+def separador():
+    print('-' * 70)
 
 if __name__ == '__main__':
-    fs.print_hi('Luko')
-    fs.print_hi('Noe')
+    data = pd.read_excel('AAPL.xlsx', sheet_name='Hoja1')
+    data_index_date = data.set_index('timestamp')
+    print(data_index_date.loc['2008-05-15'])
+    separador()
+    posteriores = data_index_date.loc['2008-05-15':'2020-03-06']
+    print(posteriores.tail().iloc[2:5])
+    separador()
+    posteriores
