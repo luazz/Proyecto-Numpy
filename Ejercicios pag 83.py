b4fd1ce5352d085996fb7beb58f2e3073ca97217
luazz <derosa.lucas@gmail.com>
2021-09-16 22:00:44 -0300

    Finalizado el primer ejercicio de la pagina 83

--- Ejercicios pag 83.py
+++ Ejercicios pag 83.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
+def separador():
+    print('-' * 70)
+
 data = pd.read_excel('AAPL.xlsx', sheet_name='Hoja1')
 dataIndexDate = data.set_index('timestamp')
 dataResul = dataIndexDate[['volume']].copy()
 dataResul['dif volume'] = dataResul['volume'] - (dataResul['volume'].shift(-1))
+print(dataResul)
+separador()
